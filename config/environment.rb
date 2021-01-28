5cd273d7fc4c8b9dfede4d5129bc9376cae481f8
SuperPox <calebkohl@gmail.com>
2021-01-27 23:13:15 -0700

    commit tree 5a21d1b15a5524bbc84f5ee68f4faefbb8332f2b

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,3 @@
-# Set up Bundler here following the instructions in the README
+# Set up Bundler here following the instructions in the 
+require 'bundler/setup'
+Bundler.require(:default, :development) 
